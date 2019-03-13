970bb85761dc8e5bb178a13ac528fbb82a392323
orjerby <orjerby@gmail.com>
2019-03-13 15:56:03 +0200

    fix

--- server/src/app.js
+++ server/src/app.js
@@ -5,12 +5,12 @@ const userRouter = require('./routers/user')
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, '..', '..', 'client', 'build')))
+//app.use(express.static(path.join(__dirname, '..', '..', 'client', 'build')))
 app.use(express.json())
 app.use(userRouter)
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', '..', 'client', 'build', 'index.html'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '..', '..', 'client', 'build', 'index.html'));
+// });
 
 module.exports = app
